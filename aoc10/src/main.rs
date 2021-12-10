53fbb44660b30ce876effdc6a2ed6fc5b68b81b5
Dan Skorupski <boardwalk@ersatsz.com>
2021-12-10 00:27:32 -0600

    Refactor day 10

--- aoc10/src/main.rs
+++ aoc10/src/main.rs
@@ -1,110 +1,70 @@
-use std::io;
+use anyhow::{bail, Result};
+use std::io::{self, BufRead};
 
 const PART2: bool = true;
 
-fn make_err(msg: String) -> io::Result<()> {
-    Err(io::Error::new(io::ErrorKind::Other, msg))
+struct Grouper {
+    open: u8,
+    close: u8,
+    inc_score: u64,
+    bad_score: u64,
 }
 
-fn main() -> io::Result<()> {
-    let mut line = String::new();
+const GROUPERS: [Grouper; 4] = [
+    Grouper { open: b'(', close: b')', inc_score: 1u64, bad_score: 3u64 },
+    Grouper { open: b'[', close: b']', inc_score: 2u64, bad_score: 57u64 },
+    Grouper { open: b'{', close: b'}', inc_score: 3u64, bad_score: 1197u64 },
+    Grouper { open: b'<', close: b'>', inc_score: 4u64, bad_score: 25137u64 },
+];
+
+fn eval_line(line: io::Result<Vec<u8>>) -> Result<u64> {
     let mut grouper_stack = Vec::new();
-    let mut scores = Vec::new();
-
-    loop {
-        io::stdin().read_line(&mut line)?;
-
-        if line.is_empty() {
-            break;
-        }
-
-        let mut line_score = 0u64;
-        let mut broken = false;
-
-        for ch in line.trim_end().chars() {
-            let score = match ch {
-                '(' | '[' | '{' | '<' => {
-                    grouper_stack.push(ch);
-                    None
-                }
-                ')' => {
-                    if grouper_stack.pop() != Some('(') {
-                        Some(3u64)
-                    } else {
-                        None
-                    }
-                }
-                ']' => {
-                    if grouper_stack.pop() != Some('[') {
-                        Some(57u64)
-                    } else {
-                        None
-                    }
-                }
-                '}' => {
-                    if grouper_stack.pop() != Some('{') {
-                        Some(1197u64)
-                    } else {
-                        None
-                    }
-                }
-                '>' => {
-                    if grouper_stack.pop() != Some('<') {
-                        Some(25137u64)
-                    } else {
-                        None
-                    }
-                }
-                _ => {
-                    return make_err(format!("unknown character {}", ch));
-                }
-            };
-
-            if let Some(score) = score {
-                if !PART2 {
-                    line_score += score;
-                }
-
-                broken = true;
+    let mut bad_score = 0;
+    for ch in line?.into_iter() {
+        if let Some(grouper) = GROUPERS.iter().find(|grouper| grouper.open == ch) {
+            grouper_stack.push(grouper);
+        } else if let Some(grouper) = GROUPERS.iter().find(|grouper| grouper.close == ch) {
+            if grouper_stack.last().map(|grouper| grouper.open) == Some(grouper.open) {
+                grouper_stack.pop();
+            } else {
+                bad_score = grouper.bad_score;
                 break;
             }
+        } else {
+            bail!("unknown character {}", ch);
         }
+    };
 
-        if !broken && PART2 {
-            while let Some(ch) = grouper_stack.pop() {
-                let score = match ch {
-                    '(' => 1u64,
-                    '[' => 2u64,
-                    '{' => 3u64,
-                    '<' => 4u64,
-                    _ => {
-                        panic!("logic error");
-                    }
-                };
-
-                line_score = line_score * 5 + score;
-            }
+    let line_score = if PART2 {
+        if bad_score == 0 {
+            // Process partial/complete line
+            grouper_stack.iter().fold(0, |accum, grouper| accum * 5 + grouper.inc_score)
+        } else {
+            // Discard broken line
+            0
         }
+    } else {
+        bad_score
+    };
 
-        if line_score != 0 {
-            scores.push(line_score);
-        }
+    Ok(line_score)
+}
 
-        line.clear();
-        grouper_stack.clear();
-    }
+fn main() -> Result<()> {
+    let mut scores = io::stdin().lock().split(b'\n').map(eval_line).collect::<Result<Vec<u64>>>()?;
+    scores.retain(|&score| score != 0);
 
-    let total_score = if PART2 {
+    let final_score = if PART2 {
         if scores.len() % 2 != 1 {
-            return make_err(format!("not an odd amount of scores"));
+            bail!("not an odd amount of scores");
         }
 
         scores.sort();
         scores[scores.len() / 2]
     } else {
-        scores.iter().copied().reduce(|x, y| x + y).unwrap_or(0)
+        scores.iter().fold(0, |accum, item| accum + item)
     };
 
-    println!("score is {}", total_score);
+    println!("final score is {}", final_score);
     Ok(())
 }
